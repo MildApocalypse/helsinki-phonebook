238586fa4d73f9ee850e0fbd51003ed149694163
MildApocalypse <lawrencebuck@outlook.com>
2024-12-27 21:04:59 +1300

    mongodb implementation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 const token = morgan.token('body', function(req, res) {return JSON.stringify(req.body)})
 
@@ -39,84 +41,98 @@ app.get('/', (request, response) => {
     )
 })
 
-app.get('/info', (request, response) => {
-    const num = phonebook.length
-    const time = new Date(Date.now())
-    response.send(
-        `<p>Phonebook has info for ${num} people</p>
-        <p>${time}</p>`
-    )
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+    .then(count =>{
+        const time = new Date(Date.now())
+        response.send(
+            `<p>Phonebook has info for ${count} people</p>
+            <p>${time}</p>`
+        )
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = phonebook.find((elem) => {return elem.id === id})
-    console.log(person)
-    if(person){
-        return response.json(person)
-    }
-    response.status(400).json({ 
-        error: 'content missing' 
+    Person.find({}).then(notes => {
+        response.json(notes)
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    phonebook = phonebook.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person){
+            response.json(person)
+        }
+        else{
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-
-    if(!body.name){
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    else if(!body.number){
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    else if(findPerson(body)){
-        return response.status(400).json({
-            error: 'person already exists'
-        })
-    }
 
     const person = {
         name: body.name,
         number: body.number,
-        id: generateID(),
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new : true })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+    
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
     }
 
-    phonebook = phonebook.concat(person)
-    response.json(person)
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    else if(error.name === 'ValidationError'){
+        return response.status(400).send({ error: error.message})
+    }
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-//===============FUNCTIONS================//
-
-const findPerson = (body) => {
-    return phonebook.find(person => person.name === body.name)
-}
-
-const generateID = () => {
-    const maxID = phonebook.length > 0
-    ? Math.max(...phonebook.map(n => Number(n.id)))
-    : 0
-    return String(maxID + 1)
-}
